71c984f9387d1d8dbae583d510c40c4c292a01fe
Priya R.S <priya.rs@noahdatatech.com>
2018-03-30 12:17:24 +0530

    pagination issue solved for strategy and home

--- src/main/webapp/app/entities/strategy-master/strategy-master-detail.controller.js
+++ src/main/webapp/app/entities/strategy-master/strategy-master-detail.controller.js
@@ -13,40 +13,40 @@
         vm.strategyMaster = entity;
         vm.previousState = previousState.name;
         vm.itemsPerPage = paginationConstants.itemsPerPage;
-        vm.predicate = pagingParams.predicate;      
+        vm.predicate = pagingParams.predicate;
         vm.reverse = true;
         vm.page = 1;
         vm.opportunitySummaryData = vm.strategyMaster.opportunitySummaryData;
-       
+
         vm.itemsValue = 'Opportunities';
-       
-       
-     
+
+
+
         var unsubscribe = $rootScope.$on('researchRepositoryLearningSystemApp:strategyMasterUpdate', function(event, result) {
             vm.strategyMaster = result;
 
             });
         $scope.$on('$destroy', unsubscribe);
-       
+
         vm.predicate = 'id';
         vm.totalItems = vm.opportunitySummaryData.length;
         vm.queryCount = vm.totalItems;
         vm.page = pagingParams.page;
-        vm.transition = transition;
-        
-        $scope.order = function (predicate) {  
-           vm.reverse = (vm.predicate === vm.predicate) ? !vm.reverse : false;  
-            vm.predicate = predicate;  
-          };  
-        
-          $scope.paginate = function (value) {  
-            var begin, end, index;  
-            begin = (vm.page - 1) * vm.itemsPerPage;  
-            end = begin + vm.itemsPerPage;  
-            index = vm.opportunitySummaryData.indexOf(value);  
-            return (begin <= index && index < end);  
+      /*  vm.transition = transition;*/
+
+        $scope.order = function (predicate) {
+           vm.reverse = (vm.predicate === vm.predicate) ? !vm.reverse : false;
+            vm.predicate = predicate;
           };
-      
+
+          $scope.paginate = function (value) {
+            var begin, end, index;
+            begin = (vm.page - 1) * vm.itemsPerPage;
+            end = begin + vm.itemsPerPage;
+            index = vm.opportunitySummaryData.indexOf(value);
+            return (begin <= index && index < end);
+          };
+
         var myDate = new Date();
 
         var previousYear = new Date(myDate);
@@ -79,13 +79,13 @@
 
         $scope.neYearNext = $filter('date')(nextYearNext,'yyyy');
 
-        function transition () {
+  /*      function transition () {
             $state.transitionTo($state.$current, {
                 page: vm.page,
                 sort: vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc'),
                 search: vm.currentSearch
             });
-        }
+        }*/
 
 
     }
