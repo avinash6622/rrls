294fa9b18613b279322ffb65142ec1cba69460dd
girija noah <girija noah@girijanoah-PC>
2018-04-30 16:49:32 +0530

    Opportunity learning validation is done

--- src/main/webapp/app/entities/opportunity-learning/opportunity-learning.controller.js
+++ src/main/webapp/app/entities/opportunity-learning/opportunity-learning.controller.js
@@ -5,21 +5,42 @@
         .module('researchRepositoryLearningSystemApp')
         .controller('OpportunityLearningController', OpportunityLearningController);
 
-    OpportunityLearningController.$inject = ['OpportunityLearning', '$scope','ParseLinks', '$uibModalInstance', 'AlertService', 'paginationConstants','options'];
+    OpportunityLearningController.$inject = ['OpportunityLearning', '$scope','Principal','ParseLinks', '$uibModalInstance', 'AlertService', 'paginationConstants','options'];
 
-    function OpportunityLearningController(OpportunityLearning,$scope, ParseLinks, $uibModalInstance, AlertService, paginationConstants,options) {
+    function OpportunityLearningController(OpportunityLearning,$scope, Principal,ParseLinks, $uibModalInstance, AlertService, paginationConstants,options) {
     	  var $ctrl=this;
     	  
     	  $ctrl.clear = clear;
     	  $ctrl.save=save;
     	  $ctrl.showLearning=false;
+    	  $ctrl.account = null;
+    	  $ctrl.opportunityMaster = options;
     	  
     	  console.log(options);
     	  OpportunityLearning.learningComment({id: options.id}, function(response) {
           	console.log(response);
           	$ctrl.learnings = response;
           });
+    	  $scope.ckOptions = {
+    	            language: 'en',
+    	            allowedContent: true,
+    	            entities: false,
+    	          
+    	        };
+    	  $scope.$on('authenticationSuccess', function() {
+              getAccount();
+          });
     	  
+    	  getAccount();
+
+          function getAccount() {
+              Principal.identity().then(function(account) {
+                  $ctrl.account = account;
+                  $ctrl.isAuthenticated = Principal.isAuthenticated;
+                  console.log('Acoount name', $ctrl.account);
+              });
+
+          }
     	  function save(){        	
           	OpportunityLearning.save({subject:$ctrl.subject,description:$ctrl.description,opportunityMaster:options},function(response){
           	console.log(response)
@@ -30,7 +51,7 @@
           	});
           }
     	  function clear () {
-          	
+    		  $ctrl.learnings=[];
               $uibModalInstance.dismiss('cancel');
           }
     }
