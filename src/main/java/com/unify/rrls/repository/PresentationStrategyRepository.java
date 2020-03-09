6b1a9ff0f76c43445ee93a9f25a9d09a61d9822d
priyasaravanan2526 <priya.saravanan@indiumsoft.com>
2020-03-09 14:43:32 +0530

    Delete api for presentation

--- src/main/java/com/unify/rrls/repository/PresentationStrategyRepository.java
+++ src/main/java/com/unify/rrls/repository/PresentationStrategyRepository.java
@@ -6,8 +6,10 @@ import com.unify.rrls.domain.StrategyMaster;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 public interface PresentationStrategyRepository extends JpaRepository<PresentationStrategyMapping, Long>  {
@@ -18,5 +20,11 @@ public interface PresentationStrategyRepository extends JpaRepository<Presentati
 
     @Query(value="select count(*) FROM presentation_strategy_mapping where strategy_id =?1", nativeQuery = true)
     Integer findCountOfPresentation(Long id);
+
+    @Modifying
+    @Transactional
+
+    @Query(value="delete FROM presentation_strategy_mapping where presentation_id =?1 and strategy_id =?2", nativeQuery = true)
+    Integer deleteByPresentationIdAndStrategyId(Long presentationId,Long strategyId);
 
 }
